581d31d60818c74f76298e7b173f65e1bd35066c
inju7 <naira.edrolla@gmail.com>
2026-05-08 13:18:36 +0800

    added picture in first project

--- src/components/WorkLists.tsx
+++ src/components/WorkLists.tsx
@@ -121,7 +121,7 @@ const WorkLists = ({ onClose }: WorkListsProps) => {
             title: "Real-Time Speech Rate and Emotion Feedback System",
             description: "Built a web app that uses NLP and machine learning to measure tempo, pronunciation, fluency, and emotion in speech, improving feedback accuracy over time through self-learning models.",
             tech: ["React", "TypeScript", "Node.js", "Express", "Tailwind CSS", "PostCSS", "Chart.js", "RxJS"],
-            images: ["/assets/Real-Time Speech Rate and Emotion Feedback/placeholder.png"]
+            images: ["/assets/Real-Time Speech Rate and Emotion Feedback/drl.png"]
         },
         {
             title: "Talk.twah",
